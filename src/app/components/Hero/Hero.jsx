2d263b8f8d7e43be10cfc151c29763f4b28e26e6
RENDEV <114122187+rendy278@users.noreply.github.com>
2024-03-17 14:07:23 +0700

    Update Hero.jsx

--- src/app/components/Hero/Hero.jsx
+++ src/app/components/Hero/Hero.jsx
@@ -1,14 +1,19 @@
 import Jujutsu from "@/app/image/Jujutsu.jpg";
 import Image from "next/image";
+import Link from "next/link";
 const Hero = () => {
   return (
     <section className="relative">
-      <div className="banner text-white text-2xl font-bold text-center ">
+      <Link
+        href={`/anime/40748`}
+        className="absolute cursor-pointer banner inset-0 w-full h-96 text-white text-center font-bold text-2xl"
+      ></Link>
+      <div className="relative h-96">
         <Image
           src={Jujutsu}
-          width={300}
-          height={300}
-          className="w-full h-80 lg:h-96 -z-50  object-cover  "
+          layout="fill"
+          objectFit="cover"
+          className="absolute inset-0"
         />
       </div>
     </section>
